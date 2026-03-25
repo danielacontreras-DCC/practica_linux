a256cd1d600012171f1b136615718ddd4161e3fb
danielacontreras-DCC <daniela.contreras@ucb.edu.bo>
2026-03-25 09:09:43 -0400

    2026-03-25_03 programa adivinanza en C++

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+std::cout<<"***********************************************"<<std::endl;
+std::cout<<"   *Bienvenidos al Juego de la Adivinanza!*    "<<std::endl;
+std::cout<<"***********************************************"<<std::endl;
+
+}
